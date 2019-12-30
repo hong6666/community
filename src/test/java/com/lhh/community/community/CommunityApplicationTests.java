442c28532bd3407ca7ee9d8ae8c574b88acaa7a0
hong666 <1345455432@qq.com>
2019-12-30 16:07:29 +0800

    init repo

---
+++ src/test/java/com/lhh/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhh.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
